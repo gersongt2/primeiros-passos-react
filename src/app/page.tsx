ede18d02b6976c2191d4e6107c60b887e2156600
gerson <gersonsilva0387@gmail.com>
2024-11-21 18:09:23 -0300

    Exercicio 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,16 @@ const Page = () =>{
 
   const filtro = pessoas.filter(person => person.profissao === "Gerente")
 
+
+
+//variveis e condiçoes do primeiro exercicio
+
+  const h =  new Date().getHours();
+
   return(
 
-   
-
     <div>
+      {/* 
       <h1 >Ola mundo</h1>
       <h3>Prazer em velo senhor Enderson</h3>
       
@@ -35,9 +40,39 @@ const Page = () =>{
       </ul>
 
       {filtro &&
-      <div>
-      {filtro.map(person=><h1 key={person.id}> {person.nome} - { person.profissao} </h1>)}
-      </div>}
+        <div>
+          {filtro.map(person=><h1 key={person.id}> {person.nome} - { person.profissao} </h1>)}
+        </div>
+      }
+      */}
+
+
+
+        
+        
+
+          <div className=" grid grid-cols-1  bg-gradient-to-r from-blue-500 to-blue-950 w-screen h-screen ">
+
+              <div className=" flex items-end justify-center text-4xl text-white font-bold ">
+                {new Date().getHours()} : {new Date().getMinutes()}
+              </div>
+
+              <div>
+                <h1 className=" text-4xl text-white text-center font-bold">
+                {h >= 0 && h <12 && "Bom Dia"}
+                {h >= 12 && h <18 && "Boa tarde" }
+                {h >= 18 && h <=23 && "Boa Noite"}
+                </h1>
+              </div>  
+      
+          </div>
+            
+      
+
+
+
+
+
     </div>
   
   );
